8696d470521166f0aa61395c34cf99033238c3e3
deneap <denea.polevoi@gmail.com>
2020-06-06 11:13:38 +0300

    Lectia 1, FULLL

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Register from '@/components/Register.vue'
+//import Register from '../views/Register.vue'
 Vue.use(VueRouter)
 
   const routes = [
@@ -9,6 +10,11 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
   },
   {
     path: '/about',
