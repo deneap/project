b6f05ac7898c906169192a3acac44df293393f0a
deneap <denea.polevoi@gmail.com>
2020-06-15 20:45:41 +0300

    L3 full

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -10,7 +10,7 @@
             label="Email" v-model="email"
           ></v-text-field>
           <v-text-field
-            label="Password" v-model="password"
+            label="Password" type="password" v-model="password"
           ></v-text-field>
           <div class="error" v-html="error" /><br />
           <v-btn class="cyan" @click="login" dark>Login</v-btn>
@@ -36,10 +36,12 @@ data () {
   methods: {
     async login () {
       try {
-        await AuthenticationService.login({
+        const response = await AuthenticationService.login({
           email: this.email,
           password: this.password
         })
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
       } catch (error) {
         this.error = error.response.data.error
       }
