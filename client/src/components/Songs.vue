8c9de73a56a2ded68cca0b387ddb2bed62b817b9
deneap <denea.polevoi@gmail.com>
2020-06-16 21:03:04 +0300

    L4 34min

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,7 +2,11 @@
   <v-layout justify-center>
 <v-form fluid style="margin: 1px; padding: 1px; align: middle; width: 100% ">
    <panel title="Songs">
-<h3> yo yoy yo</h3>
+     <div v-for="song in songs" :key="song.titel">
+       {{song.title}} - 
+       {{song.artists}} - 
+       {{song.album}}
+     </div>
 
      </panel>
   </v-form>
@@ -10,10 +14,19 @@
 </template>
 
 <script>
+import SongsServices from '@/services/SongsServices.js'
 import Panel from '@/components/Panel.vue'
 export default {
   components: {
     Panel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  async mounted () {
+     this.songs = await SongsServices.index()
   }
   }
 </script>
